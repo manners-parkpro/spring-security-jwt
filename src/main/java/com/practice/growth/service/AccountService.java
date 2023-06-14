ebb1d814f2e710a190dc365039283f2536fa55c9
Colin <jongsuk.park@lulumedic.com>
2023-06-14 11:39:20 +0900

    session login process 개발

--- src/main/java/com/practice/growth/service/AccountService.java
+++ src/main/java/com/practice/growth/service/AccountService.java
@@ -3,6 +3,7 @@ package com.practice.growth.service;
 import com.practice.growth.domain.dto.AccountDto;
 import com.practice.growth.domain.entity.Account;
 import com.practice.growth.exception.AlreadyEntity;
+import com.practice.growth.exception.NotFoundException;
 import com.practice.growth.exception.RequiredParamNonException;
 import com.practice.growth.exception.UserNotFoundException;
 import com.practice.growth.repository.AccountRepository;
@@ -10,6 +11,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Log4j2
 @Service
@@ -19,11 +24,20 @@ public class AccountService {
     private final AccountRepository repository;
     private final PasswordEncoder passwordEncoder;
 
+    public Account findByUsername(String username) throws NotFoundException {
+        return repository.findByUsernameIgnoreCase(username).orElseThrow(() -> new UserNotFoundException(username, UserNotFoundException.USER_NOT_FOUND));
+    }
+
+    public Account findByEmail(String email) throws NotFoundException {
+        return repository.findByEmailIgnoreCase(email).orElseThrow(() -> new UserNotFoundException(email, UserNotFoundException.USER_NOT_FOUND));
+    }
+
+    @Transactional
     public Long createUser(AccountDto dto) throws Exception {
         if (dto == null)
             throw new RequiredParamNonException("RequiredParamNonException");
 
-        if (repository.countAccountByEmail(dto.getEmail()) > 0)
+        if (repository.countByUsername(dto.getEmail()) > 0 || repository.countByEmail(dto.getUsername()) > 0)
             throw new AlreadyEntity("AlreadyEntity");
 
         Account account = new Account();
@@ -36,5 +50,11 @@ public class AccountService {
         repository.save(account);
 
         return account.getId();
+    }
+
+    @Transactional
+    public void modifyAccount(Account account) {
+        account.setLastLoginAt(LocalDateTime.now());
+        repository.save(account);
     }
 }
