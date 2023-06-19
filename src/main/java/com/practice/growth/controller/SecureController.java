d26ba4388d48d4dc3c307d358f2dc79b11e49f56
Colin <jongsuk.park@lulumedic.com>
2023-06-19 14:30:47 +0900

    commit

--- src/main/java/com/practice/growth/controller/SecureController.java
+++ src/main/java/com/practice/growth/controller/SecureController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Log4j2
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/secure/")
