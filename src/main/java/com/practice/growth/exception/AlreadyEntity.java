c189ea7284bf7a761d4957035747b732fc5936b2
Colin <jongsuk.park@lulumedic.com>
2023-06-21 16:28:12 +0900

    commit

--- src/main/java/com/practice/growth/exception/AlreadyEntity.java
+++ src/main/java/com/practice/growth/exception/AlreadyEntity.java
@@ -1,9 +1,5 @@
 package com.practice.growth.exception;
 
-/**
- * @author prographer
- * @date: 2019-04-09
- */
 public class AlreadyEntity extends Exception {
     public AlreadyEntity(String entityName) {
         super(entityName);
