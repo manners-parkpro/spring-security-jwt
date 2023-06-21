6680c2a836e9303bf3e886a0a4eafe5ba8bfdb11
Colin <jongsuk.park@lulumedic.com>
2023-06-21 18:23:53 +0900

    menu 구성중

--- src/main/java/com/practice/growth/repository/AccountRepository.java
+++ src/main/java/com/practice/growth/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.practice.growth.repository;
 
 import com.practice.growth.domain.entity.Account;
+import com.practice.growth.domain.types.YNType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     int countByEmail(String email);
 
     int countByUsernameIgnoreCase(String username);
-    Optional<Account> findByUsernameIgnoreCase(String username);
+    Optional<Account> findByUsernameIgnoreCaseAndActiveYn(String username, YNType ynType);
     Optional<Account> findByEmailIgnoreCase(String email);
     Optional<Account> findByProviderId(String providerId);
 }
