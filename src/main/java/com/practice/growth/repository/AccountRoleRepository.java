1a6c73d4000dc306545589775211e9453a9c9af2
Colin <jongsuk.park@lulumedic.com>
2023-06-22 14:30:50 +0900

    commit

--- src/main/java/com/practice/growth/repository/AccountRoleRepository.java
+++ src/main/java/com/practice/growth/repository/AccountRoleRepository.java
@@ -4,6 +4,7 @@ import com.practice.growth.domain.entity.Account;
 import com.practice.growth.domain.entity.AccountRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,6 @@ public interface AccountRoleRepository extends JpaRepository<AccountRole, Long> 
     Optional<List<AccountRole>> findByRoleNameIn(List<String> roleNames);
     Optional<AccountRole> findByRoleName(String roleName);
 
-    @Query(value = "SELECT max(sortOrder) FROM AccountRole WHERE Account = :account")
-    int getMaxSortOrder(Account account);
+    @Query(value = "SELECT max(sortOrder) FROM AccountRole WHERE account = :account")
+    int getMaxSortOrder(@Param(value = "account") Account account);
 }
